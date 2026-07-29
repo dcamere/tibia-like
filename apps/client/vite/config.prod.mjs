88465df73db5ad155ceb26101ffcae6d3633d0cf
dcamere <dcamere@gmail.com>
2026-07-29 17:29:36 -0500

    Added public domain whitelist

--- apps/client/vite/config.prod.mjs
+++ apps/client/vite/config.prod.mjs
@@ -39,10 +39,14 @@ export default defineConfig({
         }
     },
     server: {
-        port: 8080
+        port: 8080,
+        host: '0.0.0.0'
     },
     preview: {
-        allowedHosts: ['tibia-likeclient-production.up.railway.app']
+        host: '0.0.0.0',
+        port: 8080,
+        strictPort: true,
+        allowedHosts: ['.railway.app']
     },
     plugins: [
         phasermsg()
